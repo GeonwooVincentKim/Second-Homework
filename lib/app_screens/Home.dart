8cf0d8e830a7e58acb7ec85cbaafd15d4fc401bd
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-22 21:49:50 +0900

    Testing..

--- lib/app_screens/Home.dart
+++ lib/app_screens/Home.dart
@@ -1,128 +1,128 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_app/app_screens/discover/discover_attributes/discover_body.dart';
-import 'package:flutter_app/app_screens/main/Body.dart';
-import 'package:flutter_app/app_screens/settings/SideMenu.dart';
-import 'package:flutter_app/shared/helpers/icomoon.dart';
-// ignore: implementation_imports
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_app/widgets/expanded/body_details.dart';
-import 'package:flutter_app/widgets/expanded/divider.dart';
-import 'package:flutter_app/widgets/expanded/expanded_widgets.dart';
-
-
-class Home extends StatefulWidget{
-  final String title;
-
-  Home({Key key, this.title}) : super(key: key);
-  @override
-  State<StatefulWidget> createState()
-    => _HomeState();
-}
-
-class _HomeState extends State<Home>{
-  String _title;
-  int _selectedPage = 0;
-  List<Widget> pageList = List<Widget>();
-
-  @override
-  void initState() {
-    pageList.add(Body());
-    pageList.add(Discover());
-    _title = "Hello World";
-    super.initState();
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: SideMenu(),
-      appBar: AppBar(
-        backgroundColor: Colors.black87,
-        // : Text("MY GAMES"),
-        title: Text(_title),
-        // title: Text(widget.title),
-        centerTitle: true,
-      ),
-      // body: Body(),
-      // body: Center(
-      //   child: RaisedButton(
-      //     onPressed: () {
-      //       Navigator.push(
-      //         context,
-      //         MaterialPageRoute(builder: (context) => Discover()),
-      //       );
-      //     },
-      //   ),
-      // ),
-      // body: PageView(
-      //   pageSnapping: true,
-      //   // controller: controller,
-      //   scrollDirection: Axis.horizontal,
-      //   children: [
-      //     Body(),
-      //     DiscoverPage(),
-      //     // Container(color: Colors.indigo),
-      //     // Container(color: Colors.red,),
-      //     IndexedStack(
-      //       index: _selectedPage,
-      //       children: pageList,
-      //     ),
-      //   ],
-      // ),
-      body: IndexedStack(
-        index: _selectedPage,
-        children: pageList,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white,
-        onTap: _onItemTapped,
-        iconSize: 50,
-        items: const<BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(IconMoon.igamepad),
-            title: Text('My Games'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(IconMoon.isearch),
-            title: Text('Discover'),
-          ),
-        ],
-        // currentIndex: _selectedIndex,
-        // selectedFontSize: 15,
-        currentIndex: _selectedPage,
-        backgroundColor: Colors.black87,
-      ),
-      // body: Body_Divide_Test(),
-    );
-  }
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedPage = index;
-      switch(index){
-        case 0: {
-          Navigator.pushNamed(context, '/home');
-          _title="Hello World";
-          break;
-        }
-        case 1:{
-          Navigator.pushNamed(context, '/discover');
-          break;
-        }
-      }
-      //Navigator.pushNamed(context, '/discover');
-      // switch(index){
-      //   case 0: {_title="Hello World";}
-      //   break;
-      //   case 1: {_title="Bye World";}
-      //   break;
-      // }
-    });
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter_app/app_screens/discover/discover_attributes/discover_body.dart';
+// import 'package:flutter_app/app_screens/main/Body.dart';
+// import 'package:flutter_app/app_screens/settings/SideMenu.dart';
+// import 'package:flutter_app/shared/helpers/icomoon.dart';
+// // ignore: implementation_imports
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter_app/widgets/expanded/body_details.dart';
+// import 'package:flutter_app/widgets/expanded/divider.dart';
+// import 'package:flutter_app/widgets/expanded/expanded_widgets.dart';
+//
+//
+// class Home extends StatefulWidget{
+//   final String title;
+//
+//   Home({Key key, this.title}) : super(key: key);
+//   @override
+//   State<StatefulWidget> createState()
+//     => _HomeState();
+// }
+//
+// class _HomeState extends State<Home>{
+//   String _title;
+//   int _selectedPage = 0;
+//   List<Widget> pageList = List<Widget>();
+//
+//   @override
+//   void initState() {
+//     pageList.add(Body());
+//     pageList.add(Discover());
+//     _title = "Hello World";
+//     super.initState();
+//   }
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       drawer: SideMenu(),
+//       appBar: AppBar(
+//         backgroundColor: Colors.black87,
+//         // : Text("MY GAMES"),
+//         title: Text(_title),
+//         // title: Text(widget.title),
+//         centerTitle: true,
+//       ),
+//       // body: Body(),
+//       // body: Center(
+//       //   child: RaisedButton(
+//       //     onPressed: () {
+//       //       Navigator.push(
+//       //         context,
+//       //         MaterialPageRoute(builder: (context) => Discover()),
+//       //       );
+//       //     },
+//       //   ),
+//       // ),
+//       // body: PageView(
+//       //   pageSnapping: true,
+//       //   // controller: controller,
+//       //   scrollDirection: Axis.horizontal,
+//       //   children: [
+//       //     Body(),
+//       //     DiscoverPage(),
+//       //     // Container(color: Colors.indigo),
+//       //     // Container(color: Colors.red,),
+//       //     IndexedStack(
+//       //       index: _selectedPage,
+//       //       children: pageList,
+//       //     ),
+//       //   ],
+//       // ),
+//       body: IndexedStack(
+//         index: _selectedPage,
+//         children: pageList,
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//
+//         type: BottomNavigationBarType.fixed,
+//         selectedItemColor: Colors.white,
+//         unselectedItemColor: Colors.white,
+//         onTap: _onItemTapped,
+//         iconSize: 50,
+//         items: const<BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(IconMoon.igamepad),
+//             title: Text('My Games'),
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(IconMoon.isearch),
+//             title: Text('Discover'),
+//           ),
+//         ],
+//         // currentIndex: _selectedIndex,
+//         // selectedFontSize: 15,
+//         currentIndex: _selectedPage,
+//         backgroundColor: Colors.black87,
+//       ),
+//       // body: Body_Divide_Test(),
+//     );
+//   }
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedPage = index;
+//       switch(index){
+//         case 0: {
+//           Navigator.pushNamed(context, '/home');
+//           _title="Hello World";
+//           break;
+//         }
+//         case 1:{
+//           Navigator.pushNamed(context, '/discover');
+//           break;
+//         }
+//       }
+//       //Navigator.pushNamed(context, '/discover');
+//       // switch(index){
+//       //   case 0: {_title="Hello World";}
+//       //   break;
+//       //   case 1: {_title="Bye World";}
+//       //   break;
+//       // }
+//     });
+//   }
+// }
 
 // import 'package:flutter/material.dart';
 // import 'package:flutter_app/app_screens/settings/settings.dart';
@@ -293,4 +293,91 @@ class _HomeState extends State<Home>{
 //     // );
 //   }
 // }
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app_screens/discover/discover_attributes/discover_body.dart';
+import 'package:flutter_app/app_screens/main/Body.dart';
+import 'package:flutter_app/app_screens/settings/SideMenu.dart';
+import 'package:flutter_app/shared/helpers/icomoon.dart';
+// ignore: implementation_imports
+import 'package:flutter/src/widgets/framework.dart';
+
+
+class Home extends StatefulWidget{
+  final String title;
+
+  Home({Key key, this.title}) : super(key: key);
+  @override
+  State<StatefulWidget> createState()
+  => _HomeState();
+}
+
+class _HomeState extends State<Home>{
+  int _selectedPage = 0;
+  List<Widget> pageList = List<Widget>();
+
+  String _title;
+
+  @override
+  void initState() {
+    pageList.add(Body());
+    pageList.add(Discover());
+    _title = 'My Flutter';
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: SideMenu(),
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        title: Text("MY GAMES"),
+        // title: Text(_title),
+        // title: Text(widget.title),
+        centerTitle: true,
+      ),
+      // body: Body(),
+      body: IndexedStack(
+        index: _selectedPage,
+        children: pageList,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+        iconSize: 50,
+        items: const<BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(IconMoon.igamepad),
+            title: Text('My Games'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(IconMoon.isearch),
+            title: Text('Discover'),
+          ),
+        ],
+        //currentIndex: _selectedIndex,
+        // selectedFontSize: 15,
+        currentIndex: _selectedPage,
+        backgroundColor: Colors.black87,
+      ),
+      // body: Body_Divide_Test(),
+    );
+  }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedPage = index;
+      switch(index){
+        case 0: {_title="Hello World";}
+        break;
+        case 1: {_title="Bye World";}
+        break;
+      }
+    });
+  }
+}
 
--- lib/app_screens/discover/discover_attributes/discover_body.dart
+++ lib/app_screens/discover/discover_attributes/discover_body.dart
@@ -1,31 +1,163 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_app/app_screens/main/Body.dart';
+//
+// import 'package:flutter_app/shared/helpers/icomoon.dart';
+// import 'package:flutter_app/widgets/expanded/divider.dart';
+// import 'package:flutter_app/widgets/expanded/widgets_attribute/Discover/widget_discover.dart';
+//
+// import '../../settings/SideMenu.dart';
+//
+// class Discover extends StatefulWidget{
+//   Discover({Key key, this.title}): super(key: key);
+//   final String title;
+//   @override
+//   State<StatefulWidget> createState()
+//     => _DiscoverState();
+// }
+//
+// class _DiscoverState extends State<Discover>{
+//   int _selectedPage = 0;
+//   List<Widget> pageList = List<Widget>();
+//   int _currentIndex = 0;
+//
+//   @override
+//   void initState() {
+//     pageList.add(Body());
+//     pageList.add(Discover());
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       drawer: SideMenu(),
+//       appBar: AppBar(
+//         backgroundColor: Colors.black87,
+//         title: Text("DISCOVER"),
+//         centerTitle: true,
+//       ),
+//       // body: Body(),
+//       body: IndexedStack(
+//         index: _selectedPage,
+//         children: pageList,
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         currentIndex: _selectedPage,
+//         backgroundColor: Colors.black87,
+//         type: BottomNavigationBarType.fixed,
+//         selectedItemColor: Colors.white,
+//         unselectedItemColor: Colors.white,
+//         onTap: _onItemTapped,
+//         iconSize: 50,
+//         items: [
+//           BottomNavigationBarItem(
+//             icon: Icon(IconMoon.igamepad),
+//             title: Text('My Games'),
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(IconMoon.isearch),
+//             title: Text('Discover'),
+//           ),
+//         ],
+//         //currentIndex: _selectedIndex,
+//         // selectedFontSize: 15,
+//       ),
+//       // body: Body_Divide_Test(),
+//     );
+//   }
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedPage = index;
+//     });
+//   }
+// }
+//
+// // class DiscoverPage extends StatelessWidget{
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       body: DiscoverBody(),
+// //     );
+// //   }
+// // }
+// //
+// // class DiscoverBody extends StatelessWidget{
+// //   @override
+// //   Widget build(BuildContext context){
+// //     var itemList = [
+// //       "Title_Details_1", "Title_Details_2", "Title_Details_3"
+// //     ];
+// //     return Center(
+// //       child: Container(
+// //         padding: EdgeInsets.only(right: 5.0, top: 20.0),
+// //         alignment: Alignment.topLeft,
+// //         color: Colors.black12,
+// //         child: Column(
+// //             children: <Widget>[
+// //               Expanded(
+// //                   child: ListView.separated(
+// //                     shrinkWrap: true,
+// //                     separatorBuilder: (context, index) =>
+// //                         transparent_divider(),
+// //                     itemCount: itemList.length,
+// //                     itemBuilder: (context, index) => DiscoverBodyDetails()
+// //                   )
+// //               ),
+// //             ]
+// //         ),
+// //       ),
+// //     );
+// //   }
+// // }
+// //
+// //
+// // class DiscoverBodyDetails extends StatelessWidget{
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Container(
+// //       decoration: BoxDecoration(
+// //         borderRadius: BorderRadius.circular(10.0),
+// //       ),
+// //       height: 220,
+// //       child: Column(
+// //         children: [
+// //           Expanded(
+// //             flex: 3,
+// //             child: DiscoverUp(),
+// //           ),
+// //           Expanded(
+// //             flex: 1,
+// //             child: DiscoverDown(),
+// //           ),
+// //         ],
+// //       ),
+// //     );
+// //   }
+// // }
 import 'package:flutter/material.dart';
 import 'package:flutter_app/app_screens/main/Body.dart';
-
+import 'package:flutter_app/app_screens/settings/SideMenu.dart';
 import 'package:flutter_app/shared/helpers/icomoon.dart';
 import 'package:flutter_app/widgets/expanded/divider.dart';
 import 'package:flutter_app/widgets/expanded/widgets_attribute/Discover/widget_discover.dart';
-
-import '../../settings/SideMenu.dart';
 
 class Discover extends StatefulWidget{
   Discover({Key key, this.title}): super(key: key);
   final String title;
   @override
   State<StatefulWidget> createState()
-    => _DiscoverState();
+  => _DiscoverState();
 }
 
 class _DiscoverState extends State<Discover>{
-  int _selectedPage = 1;
+  int _selectedPage = 0;
   List<Widget> pageList = List<Widget>();
-  String _title;
+  int _currentIndex = 0;
 
   @override
   void initState() {
     pageList.add(Body());
-    pageList.add(Discover());
-    _title = 'Your Flutter';
-    print(_title);
+    pageList.add(DiscoverPage());
     super.initState();
   }
 
@@ -35,7 +167,7 @@ class _DiscoverState extends State<Discover>{
       drawer: SideMenu(),
       appBar: AppBar(
         backgroundColor: Colors.black87,
-        title: Text("tEXT"),
+        title: Text("DISCOVER"),
         centerTitle: true,
       ),
       // body: Body(),
@@ -61,7 +193,7 @@ class _DiscoverState extends State<Discover>{
             title: Text('Discover'),
           ),
         ],
-        // currentIndex: _selectedIndex,
+        //currentIndex: _selectedIndex,
         // selectedFontSize: 15,
       ),
       // body: Body_Divide_Test(),
@@ -70,84 +202,69 @@ class _DiscoverState extends State<Discover>{
   void _onItemTapped(int index) {
     setState(() {
       _selectedPage = index;
-      switch(index){
-        case 0:
-          Navigator.pushNamed(context, '/home');
-          break;
-        case 1:
-          Navigator.pushNamed(context, '/discover');
-          break;
-      }
-      //Navigator.pushNamed(context, '/discover');
-      // switch(index){
-      //   case 0: {_title="Hello World";}
-      //   break;
-      //   case 1: {_title="Bye World";}
-      //   break;
-      // }
     });
   }
 }
 
-// class DiscoverPage extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: DiscoverBody(),
-//     );
-//   }
-// }
-//
-// class DiscoverBody extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context){
-//     var itemList = [
-//       "Title_Details_1", "Title_Details_2", "Title_Details_3"
-//     ];
-//     return Center(
-//       child: Container(
-//         padding: EdgeInsets.only(right: 5.0, top: 20.0),
-//         alignment: Alignment.topLeft,
-//         color: Colors.black12,
-//         child: Column(
-//             children: <Widget>[
-//               Expanded(
-//                   child: ListView.separated(
-//                     shrinkWrap: true,
-//                     separatorBuilder: (context, index) =>
-//                         transparent_divider(),
-//                     itemCount: itemList.length,
-//                     itemBuilder: (context, index) => DiscoverBodyDetails()
-//                   )
-//               ),
-//             ]
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-//
-// class DiscoverBodyDetails extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         borderRadius: BorderRadius.circular(10.0),
-//       ),
-//       height: 220,
-//       child: Column(
-//         children: [
-//           Expanded(
-//             flex: 3,
-//             child: DiscoverUp(),
-//           ),
-//           Expanded(
-//             flex: 1,
-//             child: DiscoverDown(),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+class DiscoverPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DiscoverBody(),
+    );
+  }
+}
+
+class DiscoverBody extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var itemList = [
+      "Title_Details_1", "Title_Details_2", "Title_Details_3"
+    ];
+    return Center(
+      child: Container(
+        padding: EdgeInsets.only(right: 5.0, top: 20.0),
+        alignment: Alignment.topLeft,
+        color: Colors.black12,
+        child: Column(
+            children: <Widget>[
+              Expanded(
+                  child: ListView.separated(
+                      shrinkWrap: true,
+                      separatorBuilder: (context, index) =>
+                          transparent_divider(),
+                      itemCount: itemList.length,
+                      itemBuilder: (context, index) => DiscoverBodyDetails()
+                  )
+              ),
+            ]
+        ),
+      ),
+    );
+  }
+}
+
+
+class DiscoverBodyDetails extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      height: 220,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: DiscoverUp(),
+          ),
+          Expanded(
+            flex: 1,
+            child: DiscoverDown(),
+          ),
+        ],
+      ),
+    );
+  }
+}
