c6c94df2276f2a8d010291942a963ea5940e1536
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-22 21:32:04 +0900

    New initialized commit of second-homework.

---
+++ lib/widgets/expanded/divider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class divider extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return const Divider(
+      color: Colors.black,
+      height: 40,
+    );
+  }
+}
+
+// ignore: camel_case_types
+class transparent_divider extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return const Divider(
+      color: Colors.transparent,
+    );
+  }
+}
+
+// ignore: camel_case_types
+class text_divider extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return const Divider(
+      color: Colors.transparent,
+      height: 30,
+    );
+  }
+}
